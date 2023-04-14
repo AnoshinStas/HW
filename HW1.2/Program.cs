2bffc19c4c6144cc1e39dd2f6817bf57ca58f92c
Stanislav <anoshin.stanislav@yandex.ru>
2023-04-14 10:51:10 +0500

    HW1.2

---
+++ HW1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число");
+int number2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число");
+int number3 = int.Parse(Console.ReadLine());
+
+int Max = number1;
+if(number1 < number2) Max = number2;
+if(number2 < number3) Max = number3;
+System.Console.WriteLine($"Max {Max}");
