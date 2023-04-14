106ea94f353fd8bf800d4ed452a57468e6c8ae94
Stanislav <anoshin.stanislav@yandex.ru>
2023-04-14 11:11:37 +0500

    HW1.3

---
+++ hW1.3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
